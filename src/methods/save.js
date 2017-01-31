c09edab058fd75858100850de609ef866e512ce3
laggingreflex <laggingreflex@gmail.com>
2017-01-31 12:05:36 +0530

    fix: prompt
    allow passing options as config
    
    better error message

--- src/methods/save.js
+++ src/methods/save.js
@@ -56,28 +56,25 @@ export default async function (...args) {
   const deletedLength = Object.keys(deletedContent).length
   const changedLength = currentLength - previousLength
 
-  if (changedLength !== 0 && (opts.saveStrict || config.opts.saveStrict)) {
+  if (changedLength !== 0 && (opts.saveStrict || config.opts.saveStrict || config.get('saveStrict'))) {
     const label = changedLength < 0 ? 'fewer' : 'more'
     const unsigned = Math.abs(changedLength)
     const keys = []
     keys.push(..._.map(addedContent, (value, key) => '+' + key))
     keys.push(..._.map(deletedContent, (value, key) => '-' + key))
-      /* `saveStrict` prevents adding or removing keys from config */
-    throw new
-    Error(`Cannot save: newer config has [${unsigned}] ${label} keys than the original [${keys}]`)
+    /* `saveStrict` prevents adding or removing keys from config */ throw new
+    Error(`Cannot save: newer config has [${unsigned}] ${label} keys than the original [${keys}]. Either override all settings or delete previous config file: ${config.getConfigFile({silent: true})}.`)
   }
 
-  if (deletedLength && previousLength && !(opts.saveLossy || config.opts.saveLossy)) {
-    /* To prevent accidental config wipe-out/loss. Set `saveLossy` to override */
-    throw new
-    Error(`Cannot save: newer config has [${deletedLength}] fewer keys than original [${Object.keys(deletedContent)}]. Set 'saveLossy' to override`)
+  if (deletedLength && previousLength && !(opts.saveLossy || config.opts.saveLossy || config.get('saveLossy'))) {
+    /* To prevent accidental config wipe-out/loss. Set `saveLossy` to override */ throw new
+    Error(`Cannot save: newer config has [${deletedLength}] fewer keys than original [${Object.keys(deletedContent)}]. Either override all settings, or pass '--save-lossy' argument, or delete previous config file: ${config.getConfigFile({silent: true})}.`)
   }
 
   try {
     await fs.outputFile(configFile, JSON.stringify(contents, null, 2))
   } catch (error) {
-    /* There was an error saving settings to the specified `configFile` */
-    throw new
+    /* There was an error saving settings to the specified `configFile` */ throw new
     Error(`Couldn't save to file "${configFile}". ` + error.message)
   }
   config.configFileContents = contents
