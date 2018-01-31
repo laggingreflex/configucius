1a81643ef4e65a6ad1c596b984e0423f823e9fd8
laggingreflex <laggingreflex@gmail.com>
2018-01-31 16:51:59 +0530

    src..

--- index.js
+++ index.js
@@ -1,4 +1,36 @@
-'create index';
+import autobind from 'auto-bind'
+import defaults from './defaults'
+import * as methods from './methods'
 
-export src from './src';
+class Config {
+  constructor (opts = {}) {
+    autobind(this)
 
+    const config = this
+
+    config.config = {}
+    config.file = {}
+    config.unsetKeys = []
+
+    config.opts = opts
+
+    // config.defaults = defaults
+
+    config.mergeOptions()
+    config.processArgv()
+    config.processEnv()
+
+    if (config.get('help')) {
+      console.log('yeah', config.get('help'))
+      config.printHelp(true)
+    }
+
+    config.readConfigFile({ silentReadFail: true })
+
+    return this.enableProxy()
+  }
+}
+
+Object.assign(Config.prototype, methods)
+
+export default Config
